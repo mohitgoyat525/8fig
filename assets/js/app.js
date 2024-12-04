99f78618cd97c3742b8253ac1ffc0bccbc6da70e
mohitgoyat525 <montyits71@gmail.com>
2024-12-04 13:30:50 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const headers = document.querySelectorAll(".accordion-header");
+
+  headers.forEach((header) => {
+    header.addEventListener("click", () => {
+      const content = header.nextElementSibling; 
+      const icon = header.querySelector(".icon");
+      headers.forEach((otherHeader) => {
+        const otherContent = otherHeader.nextElementSibling;
+        const otherIcon = otherHeader.querySelector(".icon");
+
+        if (otherHeader !== header) {
+          otherContent.classList.add("hidden");
+          otherContent.style.maxHeight = null;
+          otherIcon.classList.remove("active");
+          otherIcon.src = "/assets/images/svg/plus-icon.svg";
+        }
+      });
+      const isHidden = content.classList.contains("hidden");
+      content.classList.toggle("hidden");
+
+      if (isHidden) {
+        content.style.maxHeight = content.scrollHeight + "px";
+      } else {
+        content.style.maxHeight = null;
+      }
+      icon.classList.toggle("active");
+      icon.src = isHidden
+        ? "assets/images/png/minus-sign.png"
+        : "/assets/images/svg/plus-icon.svg";
+    });
+  });
+});
